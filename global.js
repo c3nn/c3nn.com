c9ad3cf0d0ecd057101c554fa1b883d9ea4b24eb
Conmann <hello@c3n.nl>
2024-11-24 18:03:18 -0600

    added transitions between pages

--- global.js
+++ global.js
@@ -1,7 +1,36 @@
-import { $, css } from "/lib/c3nnUtil.js";
+import { $, $all, css } from "/lib/c3nnUtil.js";
 
 const barsCont = $('#barsCont');
 
+//* Page transitions
+function loadingMenuOff(){
+	barsCont.attr('loading-out','');
+	barsCont.removeAttr('loading');
+	setTimeout(() => {
+		barsCont.removeAttr('loading-out')
+	}, 350);
+}
+function loadingMenuOn(){
+	barsCont.attr('loading-out','');
+	barsCont.attr('loading', '');
+	setTimeout(() => {
+		barsCont.removeAttr('loading-out')
+	}, 350);
+}
+window.trans = function(href){ // transition to a page
+	loadingMenuOn();
+    setTimeout(function() { 
+        window.location.href = href
+    }, 400)
+	return false; // stops browser from opening link
+}
+window.addEventListener('load', () => {
+	setTimeout(() => {
+		loadingMenuOff();
+	}, 100);
+});
+
+//* Bars Menu
 $(':root').addEventListener('mousedown', (e) => {
 	if(css('--isMobileDevice') == "true"){
 		if(barsCont.attr('open') != 'true' && e.clientY >= window.innerHeight - 50){
@@ -18,9 +47,9 @@ window.addEventListener('resize', () => {
 });
 
 $('#barsContactPageButton').addEventListener('click', () => {
-	if($('#barsCont').hasAttribute('about-page-open')){
-		$('#barsCont').removeAttribute('about-page-open');
+	if(barsCont.hasAttr('about-page-open')){
+		barsCont.removeAttr('about-page-open');
 	}else{
-		$('#barsCont').attr('about-page-open','')
+		barsCont.attr('about-page-open','')
 	}
 });
